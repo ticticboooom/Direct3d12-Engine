a9c457afca6d1ccb4b4a2f01870a54e1f69c91fb
Kyle Cox <kylecoxforweb@gmail.com>
2018-12-21 10:25:59 +0000

    cleaning up device resources

--- D3dEngine/DeviceResources.h
+++ D3dEngine/DeviceResources.h
@@ -4,42 +4,42 @@ namespace DX {
 	static const UINT c_frameCount = 3;		// Use triple buffering.
 	/**
 	 * @brief the resources required for the D3D device
-	 * 
+	 *
 	 */
-	class DeviceResources
+	class D3DENGINE_API DeviceResources
 	{
 	public:
-		D3DENGINE_API DeviceResources(DXGI_FORMAT backBufferFormat = DXGI_FORMAT_B8G8R8A8_UNORM, DXGI_FORMAT depthBufferFormat = DXGI_FORMAT_D32_FLOAT);
-		D3DENGINE_API void SetWindow(HWND window);
-		D3DENGINE_API void SetLogicalSize(DirectX::XMFLOAT2 logicalSize);
-		D3DENGINE_API void SetDpi(float dpi);
-		D3DENGINE_API void ValidateDevice();
-		D3DENGINE_API void Present();
-		D3DENGINE_API void WaitForGpu();
+		DeviceResources(DXGI_FORMAT backBufferFormat = DXGI_FORMAT_B8G8R8A8_UNORM, DXGI_FORMAT depthBufferFormat = DXGI_FORMAT_D32_FLOAT);
+		void SetWindow(HWND window);
+		void SetLogicalSize(DirectX::XMFLOAT2 logicalSize);
+		void SetDpi(float dpi);
+		void ValidateDevice();
+		void Present();
+		void WaitForGpu();
 		// The size of the render target, in pixels.
-		D3DENGINE_API DirectX::XMFLOAT2	GetOutputSize() const { return m_outputSize; }
+		DirectX::XMFLOAT2	GetOutputSize() const { return m_outputSize; }
 		// The size of the render target, in dips.
-		D3DENGINE_API DirectX::XMFLOAT2	GetLogicalSize() const { return m_logicalSize; }
-		D3DENGINE_API
-			D3DENGINE_API float						GetDpi() const { return m_effectiveDpi; }
-		D3DENGINE_API bool						IsDeviceRemoved() const { return m_deviceRemoved; }
-		// D3D Accessors.
-		D3DENGINE_API ID3D12Device*				GetD3DDevice() const { return m_d3dDevice.Get(); }
-		D3DENGINE_API IDXGISwapChain3*			GetSwapChain() const { return m_swapChain.Get(); }
-		D3DENGINE_API ID3D12Resource*				GetRenderTarget() const { return m_renderTargets[m_currentFrame].Get(); }
-		D3DENGINE_API ID3D12Resource*				GetDepthStencil() const { return m_depthStencil.Get(); }
-		D3DENGINE_API ID3D12CommandQueue*			GetCommandQueue() const { return m_commandQueue.Get(); }
-		D3DENGINE_API ID3D12CommandAllocator*		GetCommandAllocator() const { return m_commandAllocators[m_currentFrame].Get(); }
-		D3DENGINE_API DXGI_FORMAT					GetBackBufferFormat() const { return m_backBufferFormat; }
-		D3DENGINE_API DXGI_FORMAT					GetDepthBufferFormat() const { return m_depthBufferFormat; }
-		D3DENGINE_API D3D12_VIEWPORT				GetScreenViewport() const { return m_screenViewport; }
-		D3DENGINE_API UINT						GetCurrentFrameIndex() const { return m_currentFrame; }
+		DirectX::XMFLOAT2	GetLogicalSize() const { return m_logicalSize; }
 
-		D3DENGINE_API CD3DX12_CPU_DESCRIPTOR_HANDLE GetRenderTargetView() const
+		float						GetDpi() const { return m_effectiveDpi; }
+		bool						IsDeviceRemoved() const { return m_deviceRemoved; }
+		// D3D Accessors.
+		ID3D12Device*				GetD3DDevice() const { return m_d3dDevice.Get(); }
+		IDXGISwapChain3*			GetSwapChain() const { return m_swapChain.Get(); }
+		ID3D12Resource*				GetRenderTarget() const { return m_renderTargets[m_currentFrame].Get(); }
+		ID3D12Resource*				GetDepthStencil() const { return m_depthStencil.Get(); }
+		ID3D12CommandQueue*			GetCommandQueue() const { return m_commandQueue.Get(); }
+		ID3D12CommandAllocator*		GetCommandAllocator() const { return m_commandAllocators[m_currentFrame].Get(); }
+		DXGI_FORMAT					GetBackBufferFormat() const { return m_backBufferFormat; }
+		DXGI_FORMAT					GetDepthBufferFormat() const { return m_depthBufferFormat; }
+		D3D12_VIEWPORT				GetScreenViewport() const { return m_screenViewport; }
+		UINT						GetCurrentFrameIndex() const { return m_currentFrame; }
+
+		CD3DX12_CPU_DESCRIPTOR_HANDLE GetRenderTargetView() const
 		{
 			return CD3DX12_CPU_DESCRIPTOR_HANDLE(m_rtvHeap->GetCPUDescriptorHandleForHeapStart(), m_currentFrame, m_rtvDescriptorSize);
 		}
-		D3DENGINE_API CD3DX12_CPU_DESCRIPTOR_HANDLE GetDepthStencilView() const
+		CD3DX12_CPU_DESCRIPTOR_HANDLE GetDepthStencilView() const
 		{
 			return CD3DX12_CPU_DESCRIPTOR_HANDLE(m_dsvHeap->GetCPUDescriptorHandleForHeapStart());
 		}
